7762f31f4aea855e7fe5cb62a9ccf54f62d4a764
Claudio <10453981@polimi.it>
2019-02-24 18:05:39 -0600

    Fix of part4 script

--- dp/run_task4_wiki.sh
+++ dp/run_task4_wiki.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PID_TO_KILL="$(parallel-ssh -H follower-1 -P jps | awk '/Worker/ {print $2}')"
+
 spark-submit --master $1 ./part3_task4/page_rank_wiki_partitioned_killed.py $2 $3 $4 &
 echo "***********************"
-sleep 20
+sleep 40
 echo "***********************"
-sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
+parallel-ssh -H follower-1 -P "sudo sh -c \"sync; echo 3 > /proc/sys/vm/drop_caches\""
 echo "***********************"
-parallel-ssh -h ../slaves -P kill -9 $5
+parallel-ssh -H follower-1 -P sudo kill $PID_TO_KILL
 exit 1
